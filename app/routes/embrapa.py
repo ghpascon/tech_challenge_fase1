05bc2f1e6a48fc8eda828672478dbdb8ed3bb711
Brunodias37 <62164559+Brunodias37@users.noreply.github.com>
2025-05-22 19:47:33 -0300

    Add failover

--- app/routes/embrapa.py
+++ app/routes/embrapa.py
@@ -3,9 +3,25 @@ from app.auth.dependencies import get_current_user
 from app.helpers.embrapa import get_json, get_json2
 from app.schemas.embrapa import EmbrapaRequestAno
 import requests
+import os
 from bs4 import BeautifulSoup
 
 router = APIRouter(prefix="/embrapa", tags=["EMBRAPA"])
+
+def create_backup_file(file_name, content):
+    with open(file_name, "w") as arquivo:
+        arquivo.write(content.prettify())
+        print(f"Arquivo HTML de backup criado com sucesso.")
+
+
+def get_backup_file(file_name):
+    html = open(file_name).read()
+    return BeautifulSoup(html, "html.parser")
+
+def validate_filepath(filepath):
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+    return  filepath
 
 @router.get(
     "/producao",
@@ -25,10 +41,18 @@ def embrapa_producao(
     Returns:
         dict: Dados extraídos da tabela de produção.
     """
-    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_02"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return get_json(soup)
+    backup_file = f'app/data/embrapa_producao/backup_file_embrapa_producao_{filtros.ano}.html'
+    try:
+        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_02"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        create_backup_file(backup_file, soup)
+        return get_json(soup)
+    except:
+        print("Rota fora do ar")
+        soup = get_backup_file(backup_file)
+        return get_json(soup)
+
 
 @router.get(
     "/processamento",
@@ -47,13 +71,22 @@ def embrapa_processamento(
         dict: Dados consolidados de todas as subcategorias, incluindo total geral.
     """
     soups = []
-    for i in range(1, 5):
-        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_03&subopcao=subopt_0{i}"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        soups.append(soup)
-    soups[2]
-    
+    validate_filepath(f"app/data/embrapa_processamento/{filtros.ano}/")
+    try:
+        for i in range(1,5):
+            backup_file = f'app/data/embrapa_processamento/{filtros.ano}/backup_file_embrapa_processamento_{filtros.ano}_subopt_0{i}.html'
+            url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_03&subopcao=subopt_0{i}"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            create_backup_file(backup_file, soup)
+            soups.append(soup)
+        soups[2]
+    except:
+        for i in range(1,5):
+            backup_file = f'app/data/embrapa_processamento/{filtros.ano}/backup_file_embrapa_processamento_{filtros.ano}_subopt_0{i}.html'
+            soup =get_backup_file(backup_file)
+            soups.append(soup)
+
     json = {}
     total = 0
     for soup in soups:
@@ -67,20 +100,24 @@ def embrapa_processamento(
     "/comercializacao",
     summary="Obtem os dados de comercialização entre os anos de 1970 e 2023",
 )
-def embrapa_comercializacao(
-    filtros: EmbrapaRequestAno = Depends(), 
-    current_user: dict = Depends(get_current_user)
-):
+def embrapa_comercializacao(filtros: EmbrapaRequestAno = Depends(), current_user: dict = Depends(get_current_user)):
     """
     Consulta os dados de comercialização de vinhos da EMBRAPA para o ano especificado.
 
     Returns:
         dict: Dados extraídos da tabela de comercialização.
     """
-    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_04"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return get_json(soup)
+    backup_file = f'app/data/embrapa_comercializacao/backup_file_embrapa_comercializacaoo_{filtros.ano}.html'
+    try:
+        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_04"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        create_backup_file(backup_file, soup)
+        return get_json(soup)
+    except:
+        soup = get_backup_file(backup_file)
+        return get_json(soup)
+
 
 @router.get(
     "/importacao",
@@ -99,11 +136,20 @@ def embrapa_importacao(
         dict: Dados consolidados das subcategorias, com total geral.
     """
     soups = []
-    for i in range(1, 6):
-        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_05&subopcao=subopt_0{i}"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        soups.append(soup)
+    validate_filepath(f"app/data/embrapa_importacao/{filtros.ano}/")
+    try:
+        for i in range(1,6):
+            backup_file = f'app/data/embrapa_importacao/{filtros.ano}/backup_file_embrapa_importacao_{filtros.ano}_subopt_0{i}.html'
+            url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_05&subopcao=subopt_0{i}"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            create_backup_file(backup_file, soup)
+            soups.append(soup)
+    except:
+        for i in range(1,5):
+            backup_file = f'app/data/embrapa_importacao/{filtros.ano}/backup_file_embrapa_importacao_{filtros.ano}_subopt_0{i}.html'
+            soup = get_backup_file(backup_file)
+            soups.append(soup)
 
     json = {}
     total = 0
@@ -111,7 +157,7 @@ def embrapa_importacao(
         current_json = get_json2(soup)
         total += current_json["total"]
         json[current_json["titulo"]] = current_json
-    json["total"] = total
+    json["total"]=total
     return json
 
 @router.get(
@@ -131,11 +177,20 @@ def embrapa_exportacao(
         dict: Dados consolidados das subcategorias, com total geral.
     """
     soups = []
-    for i in range(1, 5):
-        url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_05&subopcao=subopt_0{i}"
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        soups.append(soup)
+    validate_filepath(f"app/data/embrapa_exportacao/{filtros.ano}/")
+    try:
+        for i in range(1,5):
+            backup_file = f'app/data/embrapa_exportacao/{filtros.ano}/backup_file_embrapa_exportacao_{filtros.ano}_subopt_0{i}.html'
+            url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={filtros.ano}&opcao=opt_05&subopcao=subopt_0{i}"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            create_backup_file(backup_file, soup)
+            soups.append(soup)
+    except:
+        for i in range(1, 5):
+            backup_file = f'app/data/embrapa_exportacao/{filtros.ano}/backup_file_embrapa_exportacao_{filtros.ano}_subopt_0{i}.html'
+            soup = get_backup_file(backup_file)
+            soups.append(soup)
 
     json = {}
     total = 0
